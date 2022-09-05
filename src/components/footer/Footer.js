466b04971ec682027112c08b396818c9428d08fe
Azamat Shogen <baki47hanma@gmail.com>
2022-09-05 16:21:06 -0500

    states fee pdf added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,16 @@ const Footer = () => {
 
     return (
         <div className="main-footer">
-            <h1>Im the footer</h1>
+           <div className="item contacts">
+             <h5>Contacts :</h5>
+             <p className="p1" >Phone: <span>224-772-1279</span></p>
+             <p className="p2">Emal: <span>permits@imcpermits.com</span></p>
+           </div>
+          
+           <div className="item copyright">
+            <p>Copyright © 2022 All Rights Reserved</p> 
+           </div>
+         
         </div>
     )
 }
