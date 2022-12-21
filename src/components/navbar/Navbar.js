ab55aac8d098d085246d8b1a53e7143522235b57
Azamat Shogen <baki47hanma@gmail.com>
2022-12-20 23:19:56 -0600

    server updated. collapse fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -53,8 +53,6 @@ const NavBar = () => {
                         {/* <li className="nav-item active" > */}
                         {/* TODO: If user logged in, show profile */}
 
-                      
-
                         {user && (
                             <li className="nav-item" >
                                 <NavLink className="nav-link" to="/profile">
@@ -111,88 +109,12 @@ const NavBar = () => {
                                 />
                             </li>
                          )}   
-                           
                     </ul>
                 </div>
 
         </nav>
     )
-
-    // return (
-    //     <nav className="navbar navbar-expand-lg  navbar-dark bg-dark sticky-top header">
-    //             <NavLink to="/" className="nav-link">
-    //                 <HomeIcon style={{color: 'lightblue', width: "45px", height: "45px"}}/>
-    //             </NavLink>
-    //             <h6 className="navbar-title">Invoice Generator</h6>
-    //             <button className="navbar-toggler"
-    //                     type="button" data-toggle="collapse" data-target="#navbarNav"
-    //                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    //                 <span className="navbar-toggler-icon"></span>
-    //             </button>
-
-                
-    //             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-    //             <ul className="navbar-nav">
-    //                     {/* <li className="nav-item active" > */}
-    //                     {/* TODO: If user logged in, show profile */}
-
-                      
-
-    //                     {auth.user && (
-    //                         <li className="nav-item" >
-    //                             <NavLink className="nav-link" to="/profile">
-    //                                 Profile
-    //                             </NavLink>
-    //                         </li>
-    //                     )}
-    //                    {!auth.user && (
-    //                         <li className="nav-item">
-    //                             <NavLink className="nav-link" to="/login">
-    //                                 Login
-    //                             </NavLink>
-    //                         </li>
-    //                    )}
-    //                    {!auth.user && (
-    //                     <li className="nav-item">
-    //                         <NavLink className="nav-link" to="/register">
-    //                             Register
-    //                         </NavLink>
-    //                     </li>
-    //                    )}
-    //                    {auth.user && (
-    //                         <li className="nav-item">
-    //                             <button type="button" 
-    //                             className="nav-link btn logout-btn"
-    //                             onClick={handleLogout}>
-    //                             Logout</button>
-    //                         </li>
-    //                    )}
-
-    //                      {auth.user && (
-    //                         <li className="nav-item">
-    //                             <Button  className="nav-link cart-btn" 
-    //                                      aria-controls="customized-menu"
-    //                                      aria-haspopup="true"
-    //                                      onClick={handleCartClick}>
-
-    //                             <Badge badgeContent={auth.itemCount} color="error" 
-    //                                  overlap="rectangular" showZero>
-    //                                  <AddShoppingCartIcon /> 
-    //                             </Badge>
-    //                             </Button>
-    //                             <CartDrawer anchorEl={anchorEl}
-    //                                         open={Boolean(anchorEl)}
-    //                                         onclose={handleCartClose}
-    //                                         items={auth.cart}
-    //                             />
-    //                         </li>
-    //                      )}   
-                           
-    //                 </ul>
-    //             </div>
-
-    //     </nav>
-    // )
+ 
 }
 
 export default NavBar
